232203579b1f620369949148a8984ad55247b430
rafi <rafialam@appscode.com>
2026-01-28 17:58:09 +0600

    allow cors
    Signed-off-by: rafi <rafialam@appscode.com>

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite default port
+    origin: ["http://localhost:5173", "https://manage-devs.vercel.app"],
     credentials: true,
   }),
 );
